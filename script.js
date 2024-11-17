5f84f37c306e9e8a3c623d25ce7333cf08c8cc5a
Omar Parada <omarparada89@outlook.com>
2024-11-17 17:08:55 -0500

    Modificación de estilos y logica del proyecto: dos botones adicionales

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let votesA = 0;
 let votesB = 0;
+let votesBlank = 0;
 
+// Función para registrar votos
 function vote(candidate) {
   if (candidate === "candidateA") {
     votesA++;
@@ -8,5 +10,19 @@ function vote(candidate) {
   } else if (candidate === "candidateB") {
     votesB++;
     document.getElementById("votesB").textContent = votesB;
+  } else if (candidate === "blank") {
+    votesBlank++;
+    document.getElementById("votesBlank").textContent = votesBlank;
   }
+}
+
+// Función para restaurar votos
+function resetVotes() {
+  votesA = 0;
+  votesB = 0;
+  votesBlank = 0;
+
+  document.getElementById("votesA").textContent = votesA;
+  document.getElementById("votesB").textContent = votesB;
+  document.getElementById("votesBlank").textContent = votesBlank;
 }
